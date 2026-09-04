710d6e6d2dadab8cc8686bb346a81a4db07917a9
djonatan.rocha <djonatandasilvarocha@gmail.com>
2026-09-03 22:10:44 -0300

    Front

--- src/main/java/com/example/innkeeper/InnKeeperApplication.java
+++ src/main/java/com/example/innkeeper/InnKeeperApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InnKeeperApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(InnKeeperApplication.class, args);
 
     }
